1f99338adbdf8d2b57e81a603cb573c20d45c894
Recep Darıcı <recepdarici4507@gmail.com>
2022-10-07 18:39:29 +0300

    1

---
+++ src/A_OKUL_PROJE/BatchConstrator.java
@@ -0,0 +1,16 @@
+package A_OKUL_PROJE;
+
+public class BatchConstrator {
+
+
+    public static void main(String[] args) {
+
+
+        Batch batch =new Batch();
+
+        System.out.println("batch.str1 = " + batch.str1);
+
+        batch.batch();
+
+    }
+}
---
+++ src/A_OKUL_PROJE/Runner.java
@@ -0,0 +1,12 @@
+package A_OKUL_PROJE;
+
+
+import static A_OKUL_PROJE.Islemler.girisPaneli;
+
+public class Runner {
+    public static void main(String[] args) {
+        girisPaneli();//static method import edildi
+        // Islemler.girisPaneli();//YANINA BOYLEDE YAZILABILIR YAZIN
+
+    }
+}
